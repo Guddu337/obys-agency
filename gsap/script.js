6b94aa6d39711540e8c036a2412685bca9420462
Guddu337 <guddukumar002359@gmail.com>
2024-04-13 12:37:30 +0530

    Made loader successfully

---
+++ gsap/script.js
@@ -0,0 +1,13 @@
+// gsap.to("#box",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     backgroundColor:"blue"
+// })
+gsap.from("#box",{
+    x:1000,
+    duration:2,
+    delay:1,
+    backgroundColor:"green",
+    rotate:360,
+})
